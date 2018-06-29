8bfa96406a7fa4b8b1825c0480cb50d0c8aac52c
STOLFIEDUARDO <38699786+STOLFIEDUARDO@users.noreply.github.com>
2018-06-29 17:27:06 -0300

    Trabalho

---
+++ TrabalhoRelacionar.sql
@@ -0,0 +1,32 @@
+﻿CREATE TABLE categorias(
+	id INT IDENTITY(1,1),
+	nome VARCHAR(100)
+	PRIMARY KEY(id)
+	
+);
+
+CREATE TABLE fraquezas(
+	id INT IDENTITY(1,1),
+	nome VARCHAR(100)
+	PRIMARY KEY(id)
+
+);
+
+CREATE TABLE pokemons(
+	id INT IDENTITY(1,1) PRIMARY KEY,
+	nome VARCHAR(100),
+	id_categoria INT,
+	id_fraqueza_1 INT,
+	id_fraqueza_2 INT,
+	FOREIGN KEY (id_categoria) REFERENCES categorias(id),
+	FOREIGN KEY (id_fraqueza_1) REFERENCES fraquezas(id),
+	FOREIGN KEY (id_fraqueza_2) REFERENCES fraquezas(id)
+
+);
+
+
+
+
+
+
+
